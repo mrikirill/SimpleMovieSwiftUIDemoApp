034ebea60c302c7ef8dcfd982bf1c998fb8ddcd3
Kirill Glushchenko <kirill@medapps.com.au>
2022-05-04 14:48:25 +1000

    Initial Commit

---
+++ SimpleMovie/SimpleMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMovieApp.swift
+//  SimpleMovie
+//
+//  Created by kirill on 4/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
